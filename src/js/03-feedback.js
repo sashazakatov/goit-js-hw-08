063d1a9f40819632c7a4fe4e1623c17cfabdff71
sashazakatov <58004989+sashazakatov@users.noreply.github.com>
2023-02-18 23:08:03 +0200

    add third task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
 
+const LOCALSTORAGE_KEY = "feedback-form-state";
+
+const feedbackFormRef = document.querySelector('.feedback-form');
+const feedbackFormEmailRef = document.querySelector('.feedback-form [name="email"]');
+const feedbackFormMessageRef = document.querySelector('.feedback-form [name="message"]');
+
+examinationLocalStorageData();
+
+feedbackFormRef.addEventListener('input', throttle(onFeedbackFormRefInput, 500));
+feedbackFormRef.addEventListener('submit', onfeedbackFormRefSubmit);
+
+function onFeedbackFormRefInput(){
+        const data = { 
+            email: feedbackFormEmailRef.value, 
+            message: feedbackFormMessageRef.value
+        };
+        localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(data));
+}
+function onfeedbackFormRefSubmit(event){
+    event.preventDefault();
+    console.log("email:", feedbackFormEmailRef.value);
+    console.log("message:", feedbackFormMessageRef.value);
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+    event.target.reset();
+}
+function examinationLocalStorageData(){
+    const local = localStorage.getItem(LOCALSTORAGE_KEY);
+    if(!local){
+        return;
+    }
+    try{
+        const data = JSON.parse(local);
+        const {email, message} = data;
+        feedbackFormEmailRef.value = email;
+        feedbackFormMessageRef.value = message;
+    }
+    catch(error){
+        console.log("name error:", error.name);
+        console.log("message error:", error.message);
+    }
+}
