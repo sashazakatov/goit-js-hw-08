4f9cd3dec01e78cd4cecdcf4f26d0ee1a6a4b631
sashazakatov <58004989+sashazakatov@users.noreply.github.com>
2023-02-18 22:26:11 +0200

    add first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,20 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const createListItemsMarkup = (items) => {
+    return items.map(({preview, description, original}) => {
+      return `
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" data-source="${original}">
+        </a>`; 
+    }).join('');
+}
+
+const galleryRef = document.querySelector('.gallery');
+
+galleryRef.innerHTML = createListItemsMarkup(galleryItems);
+
+new SimpleLightbox('.gallery__item', {captionsData: 'alt', captionDelay: 250, sourceAttr:"href"});
